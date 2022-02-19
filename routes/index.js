501e6803cebb0854ef0926bd8db80842478f2236
Nitin Sharma <astami@shipperstack.com>
2022-02-20 05:13:02 +0530

    NO_T added search all
    Signed-off-by: Nitin Sharma <astami@shipperstack.com>

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var router = express.Router();
 const passport = require('passport');
+const connectEnsureLogin = require('connect-ensure-login');
+const async = require('async');
+const sequelize = require('../sequelize');
+const { Op } = require("sequelize");
+const { project, builder, city } = sequelize.models;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,5 +17,28 @@ router.get('/login', function(req, res, next) {
 });
 
 router.post('/login', passport.authenticate('local', { successReturnToOrRedirect: '/users', failureRedirect: '/login' }));
+
+router.get('/searchall/:search', connectEnsureLogin.ensureLoggedIn(), async function(req, res) {
+  res.send(await async.parallel({
+    cities: callback => city.findAll({
+      where:{
+        name: {[Op.substring]: req.params.search}
+      }})
+      .then((data) => callback(null, data))
+      .catch(err => callback(null, [])),
+    projects: callback => project.findAll({
+      where:{
+        name: {[Op.substring]: req.params.search}
+      }})
+      .then((data) => callback(null, data))
+      .catch(err => callback(null, [])),
+    builders: callback => builder.findAll({
+      where:{
+        name: {[Op.substring]: req.params.search}
+      }})
+      .then((data) => callback(null, data))
+      .catch(err => callback(null, [])),
+  }));
+});
 
 module.exports = router;
